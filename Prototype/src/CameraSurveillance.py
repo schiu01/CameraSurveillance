0bdf3e15802885f000d5f347a41ed6881077ba69
Steven Chiu <schiu01@gmail.com>
2023-12-01 20:46:34 -0500

    Initial Commit for Prototype

---
+++ Prototype/src/CameraSurveillance.py
@@ -0,0 +1,56 @@
+import json as js
+import base64
+import cv2
+class CameraSurveillance:
+    def __init__(self):
+        """
+            Initialization routine for camera
+            1. Read Configuration File
+            
+        """
+        self.config = None
+        self.config_file = "../config/camera_surveillance.config"
+        self.read_config()
+
+        self.frame_size = {"width": 0, "height": 0, "channels": 3}
+        self.frame_resized = {"width": 0, "height": 0, "channels": 3}
+        self.frame_small = {"width": 0, "height": 0, "channels": 3}
+
+
+        pass
+    def start(self):
+        """
+            Starts the Camera Surveillance system
+        """
+        self.init_capture()
+    def read_config(self):
+        fd = open(self.config_file,"r")
+        self.config = js.load(fd)
+        fd.close()
+    def decode_password(self, enc_password):
+        return  base64.b64decode(str(enc_password).encode('ascii')).decode('ascii')
+    def build_rtsp_url(self):
+        user = self.config["rtsp_user"]
+        password = self.decode_password(self.config["rtsp_password"])
+        url = self.config["rtsp_host_url"]
+        return f"rtsp://{user}:{password}@{url}"
+    def init_capture(self):
+        """
+            Initialization of cap objects of VideoCapture
+            Detects Width/Height/Channels
+
+        """
+        self.cap = cv2.VideoCapture(self.build_rtsp_url())
+
+        
+        self.frame_size["width"] = int(self.cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        self.frame_size["height"] = int(self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        self.frame_size["channels"] = int(self.cap.get(cv2.CAP_PROP_VIDEO_TOTAL_CHANNELS))
+
+        self.frame_resized["width"] = int(self.frame_size["width"] * self.config["resize_ratio"])
+        self.frame_resized["height"] = int(self.frame_size["height"] * self.config["resize_ratio"])
+
+        self.frame_small["width"] = int(self.frame_size["width"] * self.config["resize_ratio"] * self.config["small_resize_ratio"])
+        self.frame_small["height"] = int(self.frame_size["height"] * self.config["resize_ratio"] * self.config["small_resize_ratio"])
+
+
---
+++ Prototype/src/prototype_test.py
@@ -0,0 +1,70 @@
+import unittest
+import cv2
+import os
+import json as js
+import base64
+from CameraSurveillance import CameraSurveillance
+
+"""
+    Camera surveillance system is built using Test Driven Development (TDD) method.
+    Project: 3070 - Final Project
+    University of London.
+    Author: Steven (Suk Ching) Chiu
+
+    About:
+    Camera Surveillance System is a computer vision enabled application for detection of vehicles and persons.
+
+    Keywords:
+    opencv, background subtraction, foreground mask, yolo, you only look once, vehicle detection, person detection, centroids
+
+    Python Open CV Install:
+    pip install opencv-python
+
+    Prototyping start date: 01-Dec-2023
+
+"""
+
+class TestCameraSurveillance(unittest.TestCase):
+    def setUp(self) -> None:
+        self.config = None
+        self.config_file = "../config/camera_surveillance.config"
+        self.camera_surveillance = CameraSurveillance()
+        return super().setUp()
+    
+    def test_config(self):
+        """
+            Test Purpose: Check if configuration file exists
+            date: 01-dec-2023
+
+        """
+        print("Testing Configuration File exists...")
+        config_file_exists = os.path.exists(self.config_file)
+        self.assertTrue(config_file_exists)
+    def test_config_values(self):
+        """
+            Test Purpose: import configuration values successfully
+            date: 01-dec-2023
+        """
+        print("Testing Configuration File values...")
+        fd = open(self.config_file,"r")
+        self.config = js.load(fd)
+        fd.close()
+        self.assertIsNotNone(self.config['rtsp_host_url'])
+        self.assertIsNotNone(self.config['rtsp_user'])
+        self.assertIsNotNone(self.config['rtsp_password'])                
+    def test_opencv_read(self):
+        """
+            Testing OpenCV Reading.
+            date: 01-dec-2023
+        """
+        print("Testing rtsp url...")
+        
+        rtsp_url = self.camera_surveillance.build_rtsp_url()
+        self.assertIsNotNone(rtsp_url)
+        self.camera_surveillance.init_capture()
+        self.assertIsNotNone(self.camera_surveillance.cap)
+        self.camera_surveillance.cap.release()
+        
+
+if __name__ == '__main__':
+    unittest.main()
