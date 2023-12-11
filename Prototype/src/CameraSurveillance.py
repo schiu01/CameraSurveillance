f9f0396d9cd5333b768067342673158c8c621f0d
Steven Chiu <schiu01@gmail.com>
2023-12-10 20:43:44 -0500

    Updated with Region of Interest rectangularbox

--- Prototype/src/CameraSurveillance.py
+++ Prototype/src/CameraSurveillance.py
@@ -21,14 +21,27 @@ class BackgroundSubtraction:
             self.bs_type = "absdiff"
         else:
             self.bs_type = bs_type
+        
+
+        ## Mixture of Gradients BS.
+        if(self.bs_type == "mog2"):
+            self.backsub = cv2.createBackgroundSubtractorMOG2(history=100)
     
 
 
     def get_fgmask(self, frame):
+        fgmask = None
         if(self.bs_type == "absdiff"):
-            return self.get_fgmask_absdiff(frame)
-        else:
-            return None
+            fgmask =  self.get_fgmask_absdiff(frame)
+        elif(self.bs_type == "mog2"):
+            ## https://docs.opencv.org/4.x/d1/dc5/tutorial_background_subtraction.html
+            fgmask =  self.get_fgmask_mog2(frame)
+
+        if(fgmask.size != 0):
+            self.pixels_changed = np.count_nonzero(fgmask)
+            self.pixels_changed_pct = int(100 * self.pixels_changed / np.size(fgmask))
+            return fgmask
+
     def get_history_indexes(self):
         idx = []
         for x in range(self.prev_history_index, self.total_history_frames):
@@ -36,12 +49,21 @@ class BackgroundSubtraction:
         for x in range(0,self.prev_history_index):
             idx.append(x)
         return idx
-    
+    def get_fgmask_mog2(self,frame):
+        resized_frame = cv2.resize(frame, (320, 180), interpolation=cv2.INTER_AREA)
+        resized_frame = cv2.GaussianBlur(resized_frame,(3,3),0) 
+        fgmask = self.backsub.apply(resized_frame)
+        ret, fgmask = cv2.threshold(fgmask, 200, 255, cv2.THRESH_BINARY)
+        fgmask = cv2.dilate(fgmask,None,iterations=2)
+        return fgmask
+
     def get_fgmask_absdiff(self, frame):
         """
             Iteration 1: The framesize was large at 1280 x 720 pixels, reduced to 320 x 180 pixels for more efficiency
             Iteration 2: Added thresholding of pixel values, all pixesl > X are converted to white pixes, others are black.
-            first setting of x = 65, some pixels that should be positive are not captured, we will set it to 50.
+            first setting of x = 65, some pixels that should be positive are not captured, we will set it to 50
+            Iteration 3: Added 5 frame differential with max difference being extracted.
+            Iteration 4: added gaussian blur to incoming frames.
 
             Issues and Resolutions added to iterations.:
             1. Sudden changes in lighting environment causing most pixels to change - to remediate this, we will set a threshold on % of pixels changed. 
@@ -54,7 +76,13 @@ class BackgroundSubtraction:
         
         """
         resized_frame = cv2.resize(frame, (320, 180), interpolation=cv2.INTER_AREA)
+
+        # ## Iteraiton 4: Added Gaussian Blur to minimize small movements.
+        resized_frame = cv2.GaussianBlur(resized_frame,(3,3),0) 
+
         if(self.prev_init_done == False):
+
+            # Iteration 3 - multi frame differentials
             for x in range(0, self.total_history_frames):
                 self.prev_history_frames.append(resized_frame)
             self.prev_frame = resized_frame
@@ -66,29 +94,38 @@ class BackgroundSubtraction:
             diff_frames.append(df)
 
         diff_frame = np.maximum(diff_frames[0], diff_frames[1])
+
+        ## Get max differential values.
         for idx in range(2,self.total_history_frames):
             diff_frame = np.maximum(diff_frame, diff_frames[idx])
             
 
-
+        ## Set threshold for any pixels > 50 to 255 and others to 0..
         ret, diff_frame = cv2.threshold(diff_frame, 50, 255, cv2.THRESH_BINARY)
+        diff_frame = cv2.dilate(diff_frame,None, iterations=2)
         if(ret):
             self.update_background_absdiff(resized_frame)
             ## Keep count on # pixels changed
-            self.pixels_changed = np.count_nonzero(diff_frame)
-            self.pixels_changed_pct = int(100 * self.pixels_changed / np.size(diff_frame))
             
 
 
-        
+
+
+        ## Test
+
+        # if(self.prev_init_done == False):
+        #     self.prev_frame = resized_frame
+        #     self.prev_init_done = True
+        # diff_frame = cv2.absdiff(self.prev_frame, resized_frame)
+        # self.update_background_absdiff(resized_frame)
         return diff_frame
         
 
 
     def update_background_absdiff(self, frame):
         self.prev_frame = frame
-        self.prev_history_frames[self.prev_history_index] = frame
-        self.prev_history_index = (self.prev_history_index + 1) % self.total_history_frames
+        # self.prev_history_frames[self.prev_history_index] = frame
+        # self.prev_history_index = (self.prev_history_index + 1) % self.total_history_frames
 
         
 
@@ -112,6 +149,7 @@ class CameraSurveillance:
         self.background_mask = BackgroundSubtraction(bs_type=self.config["background_subtraction_method"])
         
         ## Set Previous Frame Value = for Absolute Difference
+        self.save_video = self.config.get("save_video")
         self.datetime = datetime.now().strftime("%Y%m%d")
         self.output_file = f"raw_capture_{self.datetime}.mp4"
 
@@ -143,7 +181,14 @@ class CameraSurveillance:
             Detects Width/Height/Channels
 
         """
-        self.cap = cv2.VideoCapture(self.build_rtsp_url())
+        self.cap = None
+        if self.config.get("source") == "rtsp":
+            self.cap = cv2.VideoCapture(self.build_rtsp_url())
+        elif self.config.get("source") == "video_file":
+            self.cap =  cv2.VideoCapture(self.config.get("video_file"))
+            print(self.cap.get(cv2.CAP_PROP_FPS))
+            self.cap.set(cv2.CAP_PROP_FPS, 5)
+            print(self.cap.get(cv2.CAP_PROP_FPS))
 
         
         self.frame_size["width"] = int(self.cap.get(cv2.CAP_PROP_FRAME_WIDTH))
@@ -156,7 +201,13 @@ class CameraSurveillance:
         self.frame_small["width"] = self.config["small_frame_w"]
         self.frame_small["height"] = self.config["small_frame_h"]
         four_cc = cv2.VideoWriter_fourcc(*'mp4v')
-        self.record_out = cv2.VideoWriter(self.output_file, four_cc, 17.0, (self.frame_size["width"],self.frame_size["height"]))
+
+        self.fgmask_ratio = self.frame_resized["width"] / 320 
+
+        if(self.save_video):
+            self.record_out = cv2.VideoWriter(self.output_file, four_cc, 17.0, (self.frame_size["width"],self.frame_size["height"]))
+        else:
+            self.record_out = None
 
     def retrieve_frame(self):
         """
@@ -171,6 +222,8 @@ class CameraSurveillance:
         while(not self.camera_stop):
             ret, frame = self.retrieve_frame()
             if(ret):
+                if(frame.size == 0):
+                    break
                 self.process_frame(frame)
 
 
@@ -186,7 +239,28 @@ class CameraSurveillance:
 
         resized_frame, small_frame, gray_frame = self.augment_frame(frame)
         fg_mask = self.background_mask.get_fgmask(gray_frame)
+
         #print(f"% Pixel Change: {self.background_mask.pixels_changed_pct}%")
+
+        ## After get fgmask, we need to identify all the blobs on the screen.
+        ### We will use opencv's contours
+        contours, hierarchy = cv2.findContours(fg_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        for i, ctr in enumerate(contours):
+            ctr_area = cv2.contourArea(ctr)
+            if(ctr_area > 100):
+                if(hierarchy[0][i][3] == -1):
+                    (cx, cy, cw, ch) = cv2.boundingRect(ctr)
+                    cx = int(cx * self.fgmask_ratio)
+                    cy = int(cy * self.fgmask_ratio)
+                    cw = int(cw * self.fgmask_ratio)
+                    ch = int(ch * self.fgmask_ratio)
+                    cv2.rectangle(resized_frame, (cx, cy), (cx+cw, cy+ch), (0,0,255), 2 )
+
+                
+
+
+
+
 
         ## Use an area at bottom of window to show fgmask and frame stats
         resized_frame[self.frame_resized["height"]-fg_mask.shape[0]:self.frame_resized["height"], 0:fg_mask.shape[1]] = cv2.cvtColor(fg_mask,cv2.COLOR_GRAY2BGR)
@@ -215,7 +289,8 @@ class CameraSurveillance:
                     1)
         self.show_window(resized_frame)
         
-        self.record_out.write(frame)
+        if self.save_video:
+            self.record_out.write(frame)
         pass
     def augment_frame(self, frame):
         """
@@ -233,7 +308,8 @@ class CameraSurveillance:
             cv2.destroyAllWindows()
             self.cap.release()
             self.camera_stop = True
-            self.record_out.release()
+            if(self.save_video):
+                self.record_out.release()
 
 
 
